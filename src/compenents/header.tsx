ddf3cf3d594b7607bc3ec366bb33ef7c7e82e039
2ndr9 <rk17tnd@gmail.com>
2022-05-02 21:03:08 +0900

    ナビバースマホ対応

--- src/compenents/header.tsx
+++ src/compenents/header.tsx
@@ -20,8 +20,8 @@ const Header = () => {
   `);
 
   return (
-    <header className="border-b-2">
-      <nav className="flex justify-center my-5 lg:my-8">
+    <header className="border-b-2 text-center">
+      <nav className="my-5 lg:my-8 overflow-x-auto">
         {data.allMdx.nodes.map((n, key) => {
           return (
             <Link className="text-lg px-2 lg:px-4 m-0 lg:m-3" key={key} to={`/#${n.frontmatter!.sectionID!}`}>
