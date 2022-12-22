579b8fd32558a2687ec84ea216b1988d4484666a
2ndr9 <rk17tnd@gmail.com>
2022-12-22 13:23:00 +0900

    アイコン拡大バグ対処

--- src/compenents/layout.tsx
+++ src/compenents/layout.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from "react";
 
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false;
+
 const Layout: FC = ({ children }) => {
   return (
     <div className="mx-10 max-w-screen-lg lg:m-auto">
