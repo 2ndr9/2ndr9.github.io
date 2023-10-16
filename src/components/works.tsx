fad8a08212db478e1d1db3fc71848f15026a6447
2ndr9 <rk17tnd@gmail.com>
2023-10-16 14:22:11 +0900

    up works order algorithm

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -10,20 +10,16 @@ type WorksProps = {
 };
 
 const Works: FC<WorksProps> = ({ works }) => {
-  // positionに小数点を許容したいが，cssのorderプロパティは小数点を受け付けないため，
-  // frontmatter__positionでsortして取得したworksの自然数の順番を保持する．
-  let position_order_count = 0;
-
   return (
     <ul className="overflow-x-scroll flex">
       {works.map((work, key) => {
-        position_order_count++;
         const { body } = work.node;
+
         return (
           <li
             key={key}
             className={`mr-8 relative pb-4 flex-shrink-0 w-4/6 md:w-2/4 lg:w-2/5 prose prose-sm text-center`}
-            style={{ order: position_order_count }}
+            style={{ order: key }}
           >
             <MDXRenderer>{body}</MDXRenderer>
           </li>
