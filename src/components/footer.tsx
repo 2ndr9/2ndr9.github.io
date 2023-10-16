2f6be8797e6c508d051f40097e0b750c6f0e54c8
2ndr9 <rk17tnd@gmail.com>
2023-10-16 15:47:37 +0900

    delete privacy and up footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
         <Link className="text-lg px-4 m-3" to="/" aria-label="home">
           home
         </Link>
-        <Link className="text-lg px-4 m-3" to="/privacy">
-          privacy
+        <Link className="text-lg px-4 m-3" to="https://github.com/2ndr9/2ndr9.github.io">
+          source
         </Link>
       </div>
     </footer>
