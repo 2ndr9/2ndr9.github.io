a9f13c00f9a783d6e29ae47547d742cbff932f14
2ndr9 <rk17tnd@gmail.com>
2023-10-16 14:01:33 +0900

    collaborate code of section and aggregate into index.tsx

--- src/compenents/sections/works.tsx
+++ src/compenents/sections/works.tsx
@@ -1,58 +1,35 @@
-import React from "react";
+import React, { FC } from "react";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { graphql, useStaticQuery } from "gatsby";
-import Section from "./common";
 
-const Works = () => {
-  const data = useStaticQuery<GatsbyTypes.WorksSectionQuery>(graphql`
-    query WorksSection {
-      mdx(fileAbsolutePath: { regex: "/sections/works/works.mdx/" }) {
-        frontmatter {
-          title
-          sectionID
-        }
-      }
-      allMdx(
-        filter: { fileAbsolutePath: { regex: "/sections/works/" }, frontmatter: { visible: { eq: true } } }
-        sort: { fields: [frontmatter___position], order: ASC }
-      ) {
-        edges {
-          node {
-            body
-            frontmatter {
-              position
-            }
-          }
-        }
-      }
-    }
-  `);
+type WorksProps = {
+  works: readonly {
+    readonly node: Pick<GatsbyTypes.Mdx, "body"> & {
+      readonly frontmatter: GatsbyTypes.Maybe<Pick<GatsbyTypes.MdxFrontmatter, "position">>;
+    };
+  }[];
+};
 
-  const { title, sectionID } = data!.mdx!.frontmatter!;
-  const works = data.allMdx.edges;
-
+const Works: FC<WorksProps> = ({ works }) => {
   // positionに小数点を許容したいが，cssのorderプロパティは小数点を受け付けないため，
   // frontmatter__positionでsortして取得したworksの自然数の順番を保持する．
   let position_order_count = 0;
 
   return (
-    <Section sectionID={sectionID!} title={title}>
-      <ul className="overflow-x-scroll flex">
-        {works.map((work, key) => {
-          position_order_count++;
-          const { body } = work.node;
-          return (
-            <li
-              key={key}
-              className={`mr-8 relative pb-4 flex-shrink-0 w-4/6 md:w-2/4 lg:w-2/5 prose prose-sm text-center`}
-              style={{ order: position_order_count }}
-            >
-              <MDXRenderer>{body}</MDXRenderer>
-            </li>
-          );
-        })}
-      </ul>
-    </Section>
+    <ul className="overflow-x-scroll flex">
+      {works.map((work, key) => {
+        position_order_count++;
+        const { body } = work.node;
+        return (
+          <li
+            key={key}
+            className={`mr-8 relative pb-4 flex-shrink-0 w-4/6 md:w-2/4 lg:w-2/5 prose prose-sm text-center`}
+            style={{ order: position_order_count }}
+          >
+            <MDXRenderer>{body}</MDXRenderer>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
